3c99d46b3863ec342529fa5edf7dc60c4d6c09d1
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-02 07:10:32 +0200

    pass SumsAll variable numbers of slices

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,10 +1,22 @@
 package arrays
 
-func ArrayIterator(arr []int) (sum int) {
+func SliceSum(arr []int) (sum int) {
 
 	for _, number := range arr {
-		sum += number		
+		sum += number
 	}
-	
+
 	return
+}
+
+func SliceSumAll(slices ...[]int) []int {
+
+	numSlices := len(slices)
+	sums := make([]int, numSlices)
+
+	for i, slice := range slices {
+		sums[i] = SliceSum(slice)
+	}
+
+	return sums
 }
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,18 +1,36 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func assertGotEqualsWant(t *testing.T, got, want int) {
+func assertSliceSumGotEqualsWant(t *testing.T, got, want int) {
 	if want != got {
 		t.Errorf("wanted %q, instead got %q", want, got)
 	}
 }
 
-func TestArrayIterator(t *testing.T) {
+func assertSliceSumAllGotEqualsWant(t *testing.T, got, want []int) {
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("wanted %q, instead got %q", want, got)
+	}
+}
+
+func TestSliceSum(t *testing.T) {
 	t.Run("return default for the type", func(t *testing.T) {
-		got := ArrayIterator([]int{4, 5, 6})
+		got := SliceSum([]int{4, 5, 6})
 		want := 15
 
-		assertGotEqualsWant(t, got, want)
+		assertSliceSumGotEqualsWant(t, got, want)
+	})
+}
+
+func TestSliceSumAll(t *testing.T) {
+	t.Run("Returns 15 for input {4,5,6}", func(t *testing.T) {
+		got := SliceSumAll([]int{4, 5, 6})
+		want := []int{15}
+
+		assertSliceSumAllGotEqualsWant(t, got, want)
 	})
 }
