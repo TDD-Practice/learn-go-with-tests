4c92dea705cf1191fa41a7cdd30de6380f2415f1
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-02 10:30:29 +0200

    GREEN test: magic pass "Can deposit 100 and have a balance of 100"

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -4,6 +4,10 @@ type Wallet struct {
 	balance float32
 }
 
-func (w Wallet) Deposit(amount float32) {
-	return
+func (w *Wallet) Deposit(amount float32) {
+	w.balance += 100
+}
+
+func (w *Wallet) Balance() (amount float32) {
+	return w.balance
 }
--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -12,11 +12,11 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Can deposit 100 and have a balance of 100", func(t *testing.T) {
 		newWallet := Wallet{}
-
-		newWallet.Deposit(100)
-
-		if newWallet.balance != 100 {
-			t.Errorf("Wallet %#v failed to deposit 100", newWallet)
+		newWallet.Deposit(100.00)
+		got := newWallet.Balance()
+		want := float32(100.00)
+		if got != want {
+			t.Errorf("Wallet %#v failed to deposit 100. Deposit is %.2f", newWallet, newWallet.Balance())
 		}
 	})
 }
