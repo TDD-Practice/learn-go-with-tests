7a7c36bce3ab6ed62946703ff6d769a3d72b1a35
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-02 08:40:23 +0200

    GREEN test: circle Area

--- structs/structs.go
+++ structs/structs.go
@@ -5,9 +5,21 @@ type Rectangle struct {
 	height float64
 }
 
+type Circle struct {
+	radius float64
+}
+
 func (rect Rectangle) Area() (area float64) {
 	return rect.height * rect.width
 }
 func (rect Rectangle) Perim() (perim float64) {
 	return 2 * (rect.height + rect.width)
+}
+
+func (circ Circle) Perim() (area float64) {
+	return 3.1416 * 2 * circ.radius
+}
+
+func (circ Circle) Area() (area float64) {
+	return 3.1416 * circ.radius * circ.radius
 }
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -20,3 +20,35 @@ func TestRectPerim(t *testing.T) {
 		t.Errorf("wanted %.2f but got %.2f", want, got)
 	}
 }
+
+func TestPolimorphicArea(t *testing.T) {
+	t.Run("Can calculate area of a Rectangle", func(t *testing.T) {
+		rectangle := Rectangle{2.00, 2.00}
+		got := rectangle.Perim()
+		want := 8.00
+
+		if want != got {
+			t.Errorf("wanted %.2f but got %.2f", want, got)
+		}
+	})
+
+	t.Run("Can calculate perimeter of a Circle", func(t *testing.T) {
+		circle := Circle{10.00}
+		got := circle.Perim()
+		want := 10.00 * 2 * 3.1416
+
+		if want != got {
+			t.Errorf("wanted %.4f but got %.4f", want, got)
+		}
+	})
+
+	t.Run("Can calculate area of a Circle", func(t *testing.T) {
+		circle := Circle{10.00}
+		got := circle.Area()
+		want := 10.00 * 10.00 * 3.1416
+
+		if want != got {
+			t.Errorf("wanted %.4f but got %.4f", want, got)
+		}
+	})
+}
