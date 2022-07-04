ad2d71104f13af3fb70f31c1630bf5c19094ee32
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-04 10:16:19 +0200

    refactor Error extraction

--- maps/maps.go
+++ maps/maps.go
@@ -4,11 +4,13 @@ import "errors"
 
 type Dictionary map[string]string
 
+var ErrorKeyNotFound = errors.New("key does not exist")
+
 func (d Dictionary) Search(key string) (value string, err error) {
 	value, ok := d[key]
 
 	if !ok {
-		return value, errors.New("key does not exist")
+		return value, ErrorKeyNotFound
 	}
 
 	return value, nil
