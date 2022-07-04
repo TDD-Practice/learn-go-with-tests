bbaca2e065502252595da491ba4a32299e77aff3
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-04 10:01:04 +0200

    RED test: "Error if looking for non-existing key"

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -14,10 +14,19 @@ func TestSearch(t *testing.T) {
 
 	t.Run("Can read a value from a map given and existing key", func(t *testing.T) {
 
-		got := dictionary.Search("test_key")
+		got, err := dictionary.Search("test_key")
 		want := "test_value"
 
 		assertValuesEquals(t, got, want)
 
 	})
+
+	t.Run("Error if looking for non-existing key", func(t *testing.T) {
+
+		got, err := dictionary.Search("no_existent_key")
+		want := "test_value"
+
+		assertValuesEquals(t, got, want)
+	})
+
 }
