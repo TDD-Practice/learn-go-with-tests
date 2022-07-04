710b0e4471361797099c0ba43e15d9fb5a205ee5
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-04 16:36:10 +0200

    Change package to main to test io.Writer flexibility

--- injection/injection.go
+++ injection/injection.go
@@ -1,10 +1,15 @@
-package injection
+package main
 
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func InjectableGreeter(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func main() {
+	InjectableGreeter(os.Stdout, "Matheus")
 }
