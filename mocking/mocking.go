9278c50ae05dc2b95d23c4370b1c97cdfff7ba91
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-05 10:56:26 +0200

    REFACTOR to take sleeper as a dynamic parameter in both main and test

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func main() {
-	Countdown(os.Stdout)
+type Sleeper interface {
+	Sleep(time.Duration)
 }
 
-func Countdown(w io.Writer) {
-	fmt.Fprintln(w, "3")
-	fmt.Fprintln(w, "2")
-	fmt.Fprintln(w, "1")
-	fmt.Fprintln(w, "0")
+type CountodownSleeper struct {
+}
+
+func (s *CountodownSleeper) Sleep(t time.Duration) {
+	time.Sleep(t)
+}
+
+func main() {
+	sleeper := CountodownSleeper{}
+	Countdown(os.Stdout, &sleeper, 3)
+}
+
+func Countdown(w io.Writer, s Sleeper, n int) {
+	for i := n; i >= 0; i-- {
+		fmt.Fprintln(w, i)
+		s.Sleep(1 * time.Second)
+	}
 }
--- mocking/mocking_test.go
+++ mocking/mocking_test.go
@@ -3,12 +3,23 @@ package main
 import (
 	"bytes"
 	"testing"
+	"time"
 )
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep(t time.Duration) {
+	s.Calls++
+}
 
 func TestCountdown(t *testing.T) {
 	t.Run("Should print the countdown from 'n' to 0", func(t *testing.T) {
 		buffer := bytes.Buffer{}
-		Countdown(&buffer)
+		spySlipper := SpySleeper{}
+
+		Countdown(&buffer, &spySlipper, 3)
 
 		got := buffer.String()
 		want := "3\n2\n1\n0\n"
