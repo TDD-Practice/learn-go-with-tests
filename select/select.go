4b206368be9618ea2296a5737ae2b937d3670b19
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-12 13:57:26 +0200

    REFACTOR Racer to call real http

--- select/select.go
+++ select/select.go
@@ -1,6 +1,26 @@
 package racing
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
 func Racer(u1, u2 string) (fasterUrl string) {
-	return "http://www.quii.dev"
+	start1 := time.Now()
+	http.Get(u1)
+	duration1 := time.Since(start1)
+	fmt.Println(u1, duration1)
+
+	start2 := time.Now()
+	http.Get(u2)
+	duration2 := time.Since(start2)
+	fmt.Println(u2, duration2)
+
+	if duration2 < duration1 {
+		return u2
+	} else {
+		return u1
+	}
 
 }
