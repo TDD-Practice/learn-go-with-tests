ab63a3cb9ec15eb61545ee9d6feb0c6749918fe9
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-13 08:25:47 +0200

    RED test: refactored tests to be tabular

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,7 +1,11 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
+/*
 func TestWalk(t *testing.T) {
 
 	expected := "Matheus"
@@ -22,5 +26,32 @@ func TestWalk(t *testing.T) {
 
 	if received[0] != expected {
 		t.Errorf("expected %q, but got %q", expected, received[0])
+	}
+}
+*/
+
+func TestWalk(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		input         interface{}
+		expectedCalls []string
+	}{
+		{
+			desc:          "single field that is 'string' type",
+			input:         struct{ Name string }{"Matheus"},
+			expectedCalls: []string{"Matheus"},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			var received []string
+			walk(tC.input, func(s string) {
+				received = append(received, s)
+			})
+			if reflect.DeepEqual(received, tC.expectedCalls) {
+				t.Errorf("got %v, want %v", received, tC.expectedCalls)
+			}
+		})
 	}
 }
